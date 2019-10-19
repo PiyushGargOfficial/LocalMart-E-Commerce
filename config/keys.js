6c48e1cf21dc8943f8af9c88f5d672c100c66014
Piyush Garg <52882017+PiyushGargOfficial@users.noreply.github.com>
2019-10-19 12:45:44 +0530

    Delete keys.js

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-const dotenv = require('dotenv');
-dotenv.config();
-
-module.exports = {
-    MongoDB_URL: `mongodb+srv://Piyush:${process.env.MongoDB_Pass}@cluster0-ux9st.mongodb.net/test?retryWrites=true&w=majority`
-}
